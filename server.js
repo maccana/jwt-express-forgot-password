d9eb0809477c10782d30b512965d4653517a864e
IT-ACA <mikeymccann@gmail.com>
2022-07-01 15:03:15 -0400

    Test change Express port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 3010;
 
 app.listen(port, () => {
     console.log(`Success! Your application is running on port ${port}.`);
