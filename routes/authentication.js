f7582984bece89813e841a3ce4b37e3afd93f62d
IT-ACA <mikeymccann@gmail.com>
2022-07-18 14:09:04 -0400

    Update api routes and ejs templates

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const Joi = require('joi');
+const User = require('../models/User');
+const mongoose = require('mongoose');
 
 // Register Route
 router.get('/register', async (req, res) => {
@@ -6,8 +9,20 @@ router.get('/register', async (req, res) => {
     res.send('Register form')
 });
 
+
 router.get('/login', (req, res) => {
     res.render('login.ejs')
+})
+
+router.post('/login', async (req, res) => {
+    console.log('USER: ', User)
+
+    // Existing email check
+    const existingUsername = await User.findOne({
+        name: req.body.name
+    });
+    if (existingUsername) return res.status(400).send('Username is taken.');
+    res.render('dash.ejs')
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const auth = require('./routes/authentication');
+config = require('./config/DB');
 const port = 3010;
 
-app.use('/auth', auth)
+app.use(express.json());
+
+mongoose.connect(config.DB).then(
+    () => {
+        console.log('Database is connected')
+    },
+    err => {
+        console.log('Can not connect to the database' + err)
+    }
+);
+
+app.use('/api', auth)
 
 app.listen(port, () => {
     console.log(`Success! Your application is running on port ${port}.`);
