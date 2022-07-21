24c2786c00af0ded5e988b330c58dc259aa27766
IT-ACA <mikeymccann@gmail.com>
2022-07-21 11:59:39 -0400

    Update authentication and login view

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,7 +1,10 @@
+const app = require('express');
 const router = require('express').Router();
 const Joi = require('joi');
 const User = require('../models/User');
 const mongoose = require('mongoose');
+let postRegistrationLoginMsg = ''
+let registerSuccess
 
 // Register Route
 router.get('/register', async (req, res) => {
@@ -10,40 +13,52 @@ router.get('/register', async (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    const existingUsername = await User.findOne({
+    const existingUser = await User.findOne({
         email: req.body.email
     });
-    console.log('USER2: ', existingUsername)
 
-    if (existingUsername) {
+    console.log('USER2: ', existingUser)
+
+    if (existingUser) {
         return res.status(400).send('Username is taken.');
     } else {
-        // res.render('login.ejs')
-        res.render('login.ejs', {
-            msg: 'Please login',
-        });
+        postRegistrationLoginMsg = 'You have registered. Please login.'
+        registerSuccess = true
+        res.redirect('/api/login');
     }
     res.send('Post registration form route')
 })
 
 router.get('/login', (req, res) => {
-    res.render('login.ejs')
+
+    // TODO: create a way to pass css for msg depending on success or fail
+    res.render('login.ejs', { msg: postRegistrationLoginMsg, status: registerSuccess })
 })
 
 router.post('/login', async (req, res) => {
     // Existing email check
     console.log('USER1: ', req.body)
 
-    const existingUsername = await User.findOne({
+    const existingUser = await User.findOne({
         email: req.body.email
     });
-    console.log('USER2: ', existingUsername)
+    console.log('USER2: ', existingUser)
 
-    if (existingUsername) {
-        return res.status(400).send('Username is taken.');
+    if (existingUser) {
+        // return res.status(400).send('Username is taken.');
+        res.render('dash.ejs', { msg: 'Welcome back ', user: existingUser })
     } else {
-        res.render('dash.ejs')
+        res.render('login.ejs', { msg: 'No account found with this email' })
     }
 })
+
+// Forgot Password
+router.get('/forgot', async (req, res) => {
+    res.render('forgot.ejs')
+});
+
+router.post('/forgot', async (req, res) => {
+    res.render('forgot-confirm.ejs')
+});
 
 module.exports = router;
